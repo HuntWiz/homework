c6844c1be3fc529b0abaf80eb9958f04c102ef93
HuntWiz <72492425+HuntWiz@users.noreply.github.com>
2024-06-27 22:54:03 +0500

    homework6.py

--- homework6.py
+++ homework6.py
@@ -3,7 +3,7 @@ print(my_dict)
 print(my_dict.get('Login'), my_dict.get('Name'))
 my_dict.update({'Name': 'Pablo',
                 'Subname': 'Mundo'})
-print(my_dict.pop('Login'))
+print('Удаленное значение:',my_dict.pop('Login'))
 print(my_dict)
 
 my_set = {1, 2, 3, 3, 'list', 'book', 'list'}
