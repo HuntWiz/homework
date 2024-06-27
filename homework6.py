fffb3a6c7ff17e6d823d9b03effdd0a8f71ec88b
HuntWiz <72492425+HuntWiz@users.noreply.github.com>
2024-06-27 22:51:02 +0500

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_dict = {'Login': 'mymommy', 'Password': 52}
+print(my_dict)
+print(my_dict.get('Login'), my_dict.get('Name'))
+my_dict.update({'Name': 'Pablo',
+                'Subname': 'Mundo'})
+print(my_dict.pop('Login'))
+print(my_dict)
+
+my_set = {1, 2, 3, 3, 'list', 'book', 'list'}
+print(my_set)
+my_set.add(5)
+my_set.add('mount')
+my_set.discard(3)
+print(my_set)
