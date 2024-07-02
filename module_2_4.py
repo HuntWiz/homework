0ca378c779dddd41d8587b4e1fba492c79f7d55a
HuntWiz <72492425+HuntWiz@users.noreply.github.com>
2024-07-02 23:40:50 +0500

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+count = 0
+next = -1
+
+for i in numbers:
+    next += 1
+    for j in range(1, numbers[next]+1):
+        if numbers[next] % j == 0:
+            count += 1
+
+            #if count > 2 or (j != 1 and j != numbers[i]):
+              #  break
+    if count == 2:
+        primes.append(numbers[next])
+    else:
+        not_primes.append(numbers[next])
+    count = 0
+
+print('Primes:', primes)
+print('not Primes:', not_primes)
