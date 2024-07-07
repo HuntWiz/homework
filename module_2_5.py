0fbb7c87f7a4363804ba614165952703f5b466ae
HuntWiz <72492425+HuntWiz@users.noreply.github.com>
2024-07-07 21:54:00 +0500

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -2,9 +2,28 @@ def get_matrix(n, m, value):
     matrix = [[value] * m for i in range(n)]
     return matrix
 
+def get_matrix_hard(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+
+
+
+
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
 print(result1)
 print(result2)
 print(result3)
+
+result1 = get_matrix_hard(2, 2, 10)
+result2 = get_matrix_hard(3, 5, 42)
+result3 = get_matrix_hard(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
+
