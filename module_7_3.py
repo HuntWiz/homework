851c75fbd0a742823f4308ad9382face7bc0e2e4
HuntWiz <72492425+HuntWiz@users.noreply.github.com>
2024-10-27 21:24:14 +0500

    Update module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -11,23 +11,49 @@ class WordsFinder:
         for i in self.file_names:
             dic = []
             with open(i, encoding='utf-8') as file:
-                s = file.readlines()
-                for k in s:
-                    k.lower() #не делает ловер и не убирает знаки, исправить!
+                s = file.read().splitlines()
+                for line in s:
                     for l in [',', '.', '=', '!', '?', ';', ':', ' - ']:
-                        k.replace(l, ' ')
-                    dic.append(k.split())
+                        if l in line:
+                            line = line.replace(l, ' ')
+                    for word in line.split():
+                        dic.append(word.lower())
+
             all_words.update({str(i): dic})
 
         return all_words
 
     def find(self, word):
-        for name, words in self.get_all_words().items():
-            for i in words:
-                if word in words:
-                    return {name: i} #еще и не ищет!
+        k = 0
+        all_files = {}
+        for file in self.file_names:
+            for name, words in self.get_all_words().items():
+                for i in words:
+                    k = k + 1
+                    if word == i:
+                        all_files.update({name: k})
+                        break
+                k = 0
+        return all_files
+
+    def count(self, word):
+        k = 0
+        all_files = {}
+        for file in self.file_names:
+            for name, words in self.get_all_words().items():
+                for i in words:
+                    if word == i:
+                        k = k + 1
+                all_files.update({name: k})
+                k = 0
+        return all_files
 
 
-finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words())
-print(finder2.find('Captain'))
+
+finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt',
+                      'Rudyard Kipling - If.txt',
+                      'Mother Goose - Monday’s Child.txt')
+print(finder1.get_all_words())
+print(finder1.find('the'))
+print(finder1.count('the'))
+
