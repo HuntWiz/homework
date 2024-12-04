3e23ca18ba7417e1a5dbde7076389e8d8d9204bc
HuntWiz <72492425+HuntWiz@users.noreply.github.com>
2024-12-05 04:23:56 +0500

    Add files via upload

--- module_13_2.py
+++ module_13_2.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = ''
+api = '7632417742:AAGsGxZ67hoES9bqmPNeaLqxSfhOKaMmtbk'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
@@ -10,11 +10,13 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 @dp.message_handler(commands=['Start'])
 async def start_message(message):
     print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 
 @dp.message_handler()
 async def all_message(message):
     print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == '__main__':
